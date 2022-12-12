bb7776e93a4ee67b69bc1076199f782b226cd65b
Roqi SB <roqisb9@gmail.com>
2022-12-12 17:18:41 +0700

    add function if else in method display

--- CircularQueue/Program.cs
+++ CircularQueue/Program.cs
@@ -72,7 +72,24 @@ namespace CircularQueues
             int FRONT_position = FRONT;
             int REAR_position = REAR;
             // checks if the queue is empty
-            
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are ........................\n");
+            if (FRONT_position <= REAR_position)
+            {
+                // traverses the queue till the last element present in an array
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "    ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+ 
+            }
         }
     }
 }
