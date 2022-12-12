25a368b9ab9c9e43334de2679ec1a9346e0f102b
Roqi SB <roqisb9@gmail.com>
2022-12-12 16:42:25 +0700

    create method insert

--- CircularQueue/Program.cs
+++ CircularQueue/Program.cs
@@ -6,5 +6,20 @@ namespace CircularQueues
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+        public Program()
+        {
+            // Initializing the value of the variables REAR and FRONT to -1 ti inndicate that the queue is initially empty
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            // this statement checks for the overflow condition
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR +1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+        }
     }
 }
